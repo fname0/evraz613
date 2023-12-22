4a3c46d3d9bc4e23ca50304924a476f0d238e0e0
fname0 <kmailfw@mail.ru>
2023-12-22 21:13:05 +0300

    сс

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,42 @@ export default function Index() {
   return (
     <div className="App" id="app">
         <Seo title="ЕВРАЗ" description="ЕВРАЗ" keywords="ЕВРАЗ"/>
+
+        <div className='main'>
+        <form>
+          <div className='logo'>
+            <img src="/logo.svg"/>
+            <div className='title'>
+              <img src="/slash.svg"/>
+              <p>АРМ диспетчера ЖД станции</p>
+            </div>
+          </div>
+          <div className='auth'>Авторизация</div>
+          <div className='sep'></div>
+          <div className='inputs'>
+            <div>
+              <p>Станция</p>
+              <select>
+                <option value='Новокузнецк Северный'>Новокузнецк Северный</option>
+                <option value='Новокузнецк Северный'>Новокузнецк Северный</option>
+                <option value='Новокузнецк Северный'>Новокузнецк Северный</option>
+                <option value='Новокузнецк Северный'>Новокузнецк Северный</option>
+              </select>
+            </div>
+            <div>
+              <p>Логин<span style={{color: '#BB2532'}}>*</span></p>
+              <input className='inpLogin'/>
+            </div>
+            <div>
+              <p>Пароль<span style={{color: '#BB2532'}}>*</span></p>
+              <input type='password' className='inpPassword'/>
+              <img className='glaz' width={24} src="/glaz.svg"/>
+            </div>
+          </div>
+          <div className='sep'></div>
+          <div className='submit'>Войти</div>
+        </form>
+      </div>
     </div>
   )
 }
